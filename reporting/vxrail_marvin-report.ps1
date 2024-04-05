fd58d7e1595c1191ae4a2f1ca38e4726c985bd59
Chris <95316894+vi-universe@users.noreply.github.com>
2024-04-05 10:16:09 +0200

    Update

--- reporting/vxrail_marvin-report.ps1
+++ reporting/vxrail_marvin-report.ps1
@@ -8,39 +8,43 @@ Email:			christian@kremer.systems
 #> 
 
 
-$vxrm = read-host "enter vxrail manager ip adress"
-$user = read-host "enter administrator account"
-$pass = read-host "enter password"
 
 import-module VxRail.API
-
-function marvin-info 
-{
-
- param(
-		
-		[string]$vxrm,
-		[string]$user,
-		[string]$pass
-    )
-
-
-  
-        Get-SystemInfo -Server $vxrm -Username $user -Password $pass | Select-Object "version","number_of_host","cluster_type","is_external_vc" | FT
-        
-        Get-Cluster -Server $vxrm -Username $user -Password $pass | Select-Object "Cluster_ID","product_type","health","operational_status" | FT
-         
-        Get-SystemClusterHosts -Server $vxrm -Username $user -Password $pass | Select-Object "host_name","appliance_id","psnt","serial_number","model","health","missing" | FT
-        
-        Get-VCenterMode -Server $vxrm -Username $user -Password $pass | FT
-                
-        Get-SupportContact -Server $vxrm -Username $user -Password $pass | Select-Object "email","company","site_id" | FT
-
-
- 
+$params = @{
+      vxrm = ""
+      user = ""
+      pass = ""
 }
 
+function Get-marvin {
 
-marvin-info -vxrm $vxrm -user $user -pass $pass
+ param(
+
+	[parameter(Mandatory = $true, HelpMessage = "vxrail manager ip address")]
+	[ValidateNotNullorEmpty()]
+	[string] $vxrm,
+	[parameter(Mandatory = $true, HelpMessage = "@vsphere.local admin")]
+	[ValidateNotNullorEmpty()]
+	[string] $user,
+	[parameter(Mandatory = $true, HelpMessage = "@vsphere.local admin password")]
+	[ValidateNotNullorEmpty()]
+	[string]$pass
+		
+)
+
+
+      Get-SystemInfo -Server $vxrm -Username $user -Password $pass | Select-Object "version","number_of_host","cluster_type","is_external_vc" | FT
+
+      Get-Cluster -Server $vxrm -Username $user -Password $pass | Select-Object "Cluster_ID","product_type","health","operational_status" | FT
+      
+      Get-SystemClusterHosts -Server $vxrm -Username $user -Password $pass | Select-Object "host_name","appliance_id","psnt","serial_number","model","health","missing" | FT
+
+      Get-VCenterMode -Server $vxrm -Username $user -Password $pass | FT
+
+      Get-SupportContact -Server $vxrm -Username $user -Password $pass | Select-Object "email","company","site_id" | FT  
+
+}
+
+Get-marvin @params
 
 
